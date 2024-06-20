36029be4abf68f5fe99a92056b3395770984b132
andersonbelli <ansersonbelli@gmail.com>
2024-06-20 20:44:39 -0300

    partial shooting

--- scenes/player.gd
+++ scenes/player.gd
@@ -10,12 +10,19 @@ func _process(delta):
 	
 	if Input.is_action_pressed("shoot"):
 		var bullet = BULLET.instantiate()
-		
-		
-		
 		bullet.position = get_position_delta()
+		# bullet.rotation_degrees = rotation_degrees
 		
-		bullet.rotation_degrees = rotation_degrees
-		bullet.apply_impulse(Vector2(),Vector2(bullet_speed, 0).rotated(rotation))
+		bullet.rotation_degrees = mouse_position.angle_to(position)
+		
+		var button_angle = position.angle_to(mouse_position)
+		
+		# bullet.apply_central_force(Vector2(),Vector2(bullet_speed, 0).rotated(button_angle))
+		
+		var direction = global_position.direction_to(global_position) # this is a unit vector (length 1)
+		var impulse = direction * 100
+		bullet.add_constant_central_force(Vector2(10, 0) * delta)
+		#bullet.apply_central_impulse(impulse)
+		
 		get_tree().get_root().add_child(bullet)
 		
