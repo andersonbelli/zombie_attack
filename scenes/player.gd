fa58117eed206f64210e1c5e426b1cd5e3ad380f
andersonbelli <ansersonbelli@gmail.com>
2024-06-21 01:48:44 -0300

    implementing timer for shooting

--- scenes/player.gd
+++ scenes/player.gd
@@ -12,28 +12,25 @@ func _physics_process(delta):
 	bullet = Bullet.instantiate()
 
 func _process(delta):
-	
 	var mouse_position = get_viewport().get_mouse_position()
 	look_at(mouse_position)
 	
-	if Input.is_action_pressed("shoot"):
-		var button_angle = position.angle_to(mouse_position)
-		var player_rotation = rotation
-		var player_rotation_degress = rotation_degrees
-		
-		var _rotation = global_rotation_degrees
-		var _rotation1 = global_rotation
-		var _rotation2 = global_position
-		var _rotation3 = mouse_position
-		var _rotation4 = mouse_position.x
-		
-		bullet.position = $AnimatedSprite2D/Marker2D.position
-		#bullet.rotation_degrees = rotation_degrees
-		bullet.rotation_degrees = global_rotation
-		
-		var direction = global_position.direction_to(mouse_position)
-		var impulse = direction * 100
-		bullet.add_constant_central_force(impulse)
-		
-		#owner.add_child(bullet)
-		add_child(bullet)
+	if $cooldown.is_stopped():
+		if Input.is_action_pressed("shoot"):
+			$cooldown.start()
+			
+			bullet.position = $AnimatedSprite2D/Marker2D.position
+			
+			#bullet.rotation_degrees = rotation_degrees
+			bullet.rotation_degrees = global_rotation
+			
+			var direction = global_position.direction_to(mouse_position)
+			var impulse = direction * 100
+			bullet.add_constant_central_force(impulse)
+			
+			#owner.add_child(bullet)
+			add_child(bullet)
+
+
+func _on_cooldown_timeout():
+	$cooldown.stop()
